8aff6e01fba73d843ef3c65e602b2fce52ed4248
wangjiale <wangjiale@shuidihuzhu.com>
2019-07-01 14:36:00 +0800

    修改文档图片地址

--- static/js/doc.js
+++ static/js/doc.js
@@ -3,13 +3,13 @@ const doc = {
     {
       'id': '1',
       'teacher': 'Happy王子乐',
-      'img': 'https://img-blog.csdnimg.cn/20190628101537789.jpg?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3UwMTI4ODgwNTI=,size_16,color_FFFFFF,t_70',
+      'img': 'http://chuantu.xyz/t6/702/1561690503x992245975.jpg',
       'title': 'redis设计与实现(第二版)',
       'remark': '本书全面而完整地讲解了Redis的内部机制与实现方式，对Redis的大多数单机功能以及所有多机功能的实现原理进行了介绍，展示了这些功能的核心数据结构以及关键的算法思想,图示丰富，描述清晰，并给出大量参考信息。通过阅读本书，读者可以快速、有效地了解Redis的内部构造以及运作机制，更好、更高效地使用Redis。本书主要分为四大部分。第一部分“数据结构与对象”介绍了Redis中的各种对象及其数据结构，并说明这些数据结构如何影响对象的功能和性能。第二部分“单机数据库的实现”对Redis实现单机数据库的方法进行了介绍，包括数据库、RDB持久化、AOF持久化、事件等。第三部分“多机数据库的实现”对Redis的Sentinel、复制、集群三个多机功能进行了介绍。第四部分“独立功能的实现”对Redis中各个相对独立的功能模块进行了介绍，涉及发布与订阅、事务、Lua脚本、排序、二进制位数组、慢查询日志、监视器等。本书作者专门维护了www.redisbook.com网站，提供带有详细注释的Redis源代码，以及本书相关的更新内容。',
       'json': [
         {
           'type': 'doc',
-          'content': 'https://img-blog.csdnimg.cn/20190627201911927.jpg'
+          'content': 'http://chuantu.xyz/t6/702/1561690503x992245975.jpg'
         },
         {
           'type': 'pan',
@@ -23,13 +23,13 @@ const doc = {
     {
       'id': '2',
       'teacher': 'Happy王子乐',
-      'img': 'https://img-blog.csdnimg.cn/20190627203806349.jpg',
+      'img': 'http://chuantu.xyz/t6/702/1561691036x2890191681.jpg',
       'title': 'RocketMQ开发指南v3.24',
       'remark': '略',
       'json': [
         {
           'type': 'doc',
-          'content': 'https://img-blog.csdnimg.cn/20190627203806349.jpg'
+          'content': 'http://chuantu.xyz/t6/702/1561691036x2890191681.jpg'
         },
         {
           'type': 'pan',
@@ -45,7 +45,7 @@ const doc = {
       'teacher': 'Happy王子乐',
       'img': '',
       'title': 'Docker入门教程',
-      'remark': '电商小程序分为三端：微信小程序展示、vue管理后台、Java逻辑后台。采用当前流行技术，包括：Html5、CSS、JS、WeUI、Vue、ElementUI、Java、MySQL、Spring-boot。',
+      'remark': '略',
       'json': [
         {
           'type': 'pan',
@@ -59,13 +59,13 @@ const doc = {
     {
       'id': '4',
       'teacher': 'Happy王子乐',
-      'img': 'https://img-blog.csdnimg.cn/2019062720441665.jpg',
+      'img': 'http://chuantu.xyz/t6/702/1561690596x3752237043.jpg',
       'title': 'Java数据结构和算法',
       'remark': '《Java数据结构和算法》（第2版）介绍了计算机编程中使用的数据结构和算法，对于在计算机应用中如何操作和管理数据以取得最优性能提供了深入浅出的讲解。全书共分为15章，分别讲述了基本概念、数组、简单排序、堆和队列、链表、递归、进阶排序、二叉树、红黑树、哈希表及图形等知识。附录中则提供了运行专题Applet和例程、相关书籍和问题解答。《Java数据结构和算法》（第2版）提供了学完一门编程语言后进一步需要知道的知识。本书所涵盖的内容通常作为大学或学院中计算机系二年级的课程，在学生掌握了编程的基础后才开始本书的学习。',
       'json': [
         {
           'type': 'doc',
-          'content': 'https://img-blog.csdnimg.cn/2019062720441665.jpg'
+          'content': 'http://chuantu.xyz/t6/702/1561690596x3752237043.jpg'
         },
         {
           'type': 'pan',
@@ -79,13 +79,13 @@ const doc = {
     {
       'id': '5',
       'teacher': 'Happy王子乐',
-      'img': 'https://img-blog.csdnimg.cn/20190627204610864.jpg',
+      'img': 'http://chuantu.xyz/t6/702/1561690706x1033347913.jpg',
       'title': '深入Java虚拟机第二版',
       'remark': '',
       'json': [
         {
           'type': 'doc',
-          'content': 'https://img-blog.csdnimg.cn/20190627204610864.jpg'
+          'content': 'http://chuantu.xyz/t6/702/1561690706x1033347913.jpg'
         },
         {
           'type': 'pan',
@@ -99,7 +99,7 @@ const doc = {
     {
       'id': '6',
       'teacher': 'Happy王子乐',
-      'img': 'https://img-blog.csdnimg.cn/20190627204610864.jpg',
+      'img': '',
       'title': '设计模式：Java语言中的应用',
       'remark': '',
       'json': [
