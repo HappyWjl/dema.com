e79db16721e0c16dcd457230be7b8fbde16f6cef
wangjiale <wangjiale@shuidihuzhu.com>
2019-07-02 16:14:45 +0800

    修改logo样式，修改图片地址

--- static/js/doc.js
+++ static/js/doc.js
@@ -3,13 +3,13 @@ const doc = {
     {
       'id': '1',
       'teacher': 'Happy王子乐',
-      'img': 'http://chuantu.xyz/t6/702/1561690503x992245975.jpg',
+      'img': '../../../static/images/doc/redissjysx.jpg',
       'title': 'redis设计与实现(第二版)',
-      'remark': '本书全面而完整地讲解了Redis的内部机制与实现方式，对Redis的大多数单机功能以及所有多机功能的实现原理进行了介绍，展示了这些功能的核心数据结构以及关键的算法思想,图示丰富，描述清晰，并给出大量参考信息。通过阅读本书，读者可以快速、有效地了解Redis的内部构造以及运作机制，更好、更高效地使用Redis。本书主要分为四大部分。第一部分“数据结构与对象”介绍了Redis中的各种对象及其数据结构，并说明这些数据结构如何影响对象的功能和性能。第二部分“单机数据库的实现”对Redis实现单机数据库的方法进行了介绍，包括数据库、RDB持久化、AOF持久化、事件等。第三部分“多机数据库的实现”对Redis的Sentinel、复制、集群三个多机功能进行了介绍。第四部分“独立功能的实现”对Redis中各个相对独立的功能模块进行了介绍，涉及发布与订阅、事务、Lua脚本、排序、二进制位数组、慢查询日志、监视器等。本书作者专门维护了www.redisbook.com网站，提供带有详细注释的Redis源代码，以及本书相关的更新内容。',
+      'remark': '本书全面而完整地讲解了Redis的内部机制与实现方式，对Redis的大多数单机功能以及所有多机功能的实现原理进行了介绍，展示了这些功能的核心数据结构以及关键的算法思想,图示丰富，描述清晰，并给出大量参考信息。通过阅读本书，读者可以快速、有效地了解Redis的内部构造以及运作机制，更好、更高效地使用Redis。本书主要分为四大部分。第一部分“数据结构与对象”介绍了Redis中的各种对象及其数据结构，并说明这些数据结构如何影响对象的功能和性能。第二部分“单机数据库的实现”对Redis实现单机数据库的方法进行了介绍，包括数据库、RDB持久化、AOF持久化、事件等。第三部分“多机数据库的实现”对Redis的Sentinel、复制、集群三个多机功能进行了介绍。第四部分“独立功能的实现”对Redis中各个相对独立的功能模块进行了介绍，涉及发布与订阅、事务、Lua脚本、排序、二进制位数组、慢查询日志、监视器等。本书作者专门维护了www.redisbook.com网站，提供带有详细注释的Redis源代码，以及本书相关的更新内容。高级开发面试，中频知识点。',
       'json': [
         {
           'type': 'doc',
-          'content': 'http://chuantu.xyz/t6/702/1561690503x992245975.jpg'
+          'content': '../../../static/images/doc/redissjysx.jpg'
         },
         {
           'type': 'pan',
@@ -23,13 +23,13 @@ const doc = {
     {
       'id': '2',
       'teacher': 'Happy王子乐',
-      'img': 'http://chuantu.xyz/t6/702/1561691036x2890191681.jpg',
+      'img': '../../../static/images/doc/rocketmqkfzn.jpg',
       'title': 'RocketMQ开发指南v3.24',
       'remark': '略',
       'json': [
         {
           'type': 'doc',
-          'content': 'http://chuantu.xyz/t6/702/1561691036x2890191681.jpg'
+          'content': '../../../static/images/doc/rocketmqkfzn.jpg'
         },
         {
           'type': 'pan',
@@ -43,10 +43,14 @@ const doc = {
     {
       'id': '3',
       'teacher': 'Happy王子乐',
-      'img': '',
+      'img': '../../../static/images/doc/dockerrmjc.jpg',
       'title': 'Docker入门教程',
-      'remark': '略',
+      'remark': 'Docker 是一个开源的应用容器引擎，让开发者可以打包他们的应用以及依赖包到一个可移植的镜像中，然后发布到任何流行的 Linux或Windows 机器上，也可以实现虚拟化。容器是完全使用沙箱机制，相互之间不会有任何接口。此书籍是Docker的入门教程，容易上手，还请仔细阅读',
       'json': [
+        {
+          'type': 'doc',
+          'content': '../../../static/images/doc/dockerrmjc.jpg'
+        },
         {
           'type': 'pan',
           'url': 'https://pan.baidu.com/s/1h7U972Pc5suZJD6XgWWcyg',
@@ -59,13 +63,13 @@ const doc = {
     {
       'id': '4',
       'teacher': 'Happy王子乐',
-      'img': 'http://chuantu.xyz/t6/702/1561690596x3752237043.jpg',
+      'img': '../../../static/images/doc/javasjjgysf.jpg',
       'title': 'Java数据结构和算法',
       'remark': '《Java数据结构和算法》（第2版）介绍了计算机编程中使用的数据结构和算法，对于在计算机应用中如何操作和管理数据以取得最优性能提供了深入浅出的讲解。全书共分为15章，分别讲述了基本概念、数组、简单排序、堆和队列、链表、递归、进阶排序、二叉树、红黑树、哈希表及图形等知识。附录中则提供了运行专题Applet和例程、相关书籍和问题解答。《Java数据结构和算法》（第2版）提供了学完一门编程语言后进一步需要知道的知识。本书所涵盖的内容通常作为大学或学院中计算机系二年级的课程，在学生掌握了编程的基础后才开始本书的学习。',
       'json': [
         {
           'type': 'doc',
-          'content': 'http://chuantu.xyz/t6/702/1561690596x3752237043.jpg'
+          'content': '../../../static/images/doc/javasjjgysf.jpg'
         },
         {
           'type': 'pan',
@@ -79,13 +83,13 @@ const doc = {
     {
       'id': '5',
       'teacher': 'Happy王子乐',
-      'img': 'http://chuantu.xyz/t6/702/1561690706x1033347913.jpg',
+      'img': '../../../static/images/doc/javajvm.jpg',
       'title': '深入Java虚拟机第二版',
-      'remark': '',
+      'remark': 'JVM是Java Virtual Machine（Java虚拟机）的缩写，JVM是一种用于计算设备的规范，它是一个虚构出来的计算机，是通过在实际的计算机上仿真模拟各种计算机功能来实现的。高级开发面试，高频知识点。',
       'json': [
         {
           'type': 'doc',
-          'content': 'http://chuantu.xyz/t6/702/1561690706x1033347913.jpg'
+          'content': '../../../static/images/doc/javajvm.jpg'
         },
         {
           'type': 'pan',
@@ -101,7 +105,7 @@ const doc = {
       'teacher': 'Happy王子乐',
       'img': '',
       'title': '设计模式：Java语言中的应用',
-      'remark': '',
+      'remark': '设计模式（Design Pattern）是一套被反复使用、多数人知晓的、经过分类的、代码设计经验的总结。使用设计模式的目的：为了代码可重用性、让代码更容易被他人理解、保证代码可靠性。 设计模式使代码编写真正工程化；设计模式是软件工程的基石脉络，如同大厦的结构一样。高级开发面试，高频知识点。',
       'json': [
         {
           'type': 'pan',
