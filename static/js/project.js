4766172c74b8934e2fd90dad184751a5d08def44
wangjiale <wangjiale@shuidihuzhu.com>
2019-07-03 20:30:27 +0800

    修改内容图片

--- static/js/project.js
+++ static/js/project.js
@@ -3,98 +3,121 @@ const project = {
     {
       'id': '1',
       'teacher': 'Happy王子乐',
-      'img': '../../../static/images/dsxcx/dsxcx.jpg',
-      'title': '微信小程序登录Demo',
-      'remark': '电商小程序分为三端：微信小程序展示、vue管理后台、Java逻辑后台。采用当前流行技术，包括：Html5、CSS、JS、WeUI、Vue、ElementUI、Java、MySQL、Spring-boot。',
+      'img': '../../../static/images/project/es-home/es-home1.jpg',
+      'title': '企业级搜索系统入门',
+      'remark': 'es-home项目是一套搜索系统，包括 同步历史数据、准实时同步数据、条件搜索数据、智能生成代码 四个模块。 采用JAVA语言，并结合elasticsearch、canal、MySQL实现数据搜索、同步数据。',
       'json': [
         {
           'type': 'project',
-          'url': 'https://pan.baidu.com/s/1wQ6TeSnm1_zIQVl6A211Lg',
-          'password': '77um'
+          'content': 'https://github.com/HappyWjl/es-home'
+        },
+        {
+          'type': 'img_big',
+          'content': ['../../../static/images/project/es-home/es-home1.jpg', 
+                      '../../../static/images/project/es-home/es-home2.jpg', 
+                      '../../../static/images/project/es-home/es-home3.jpg']
         }
       ],
-      'price': '600',
-      'time': '2019-06-01'
+      'time': '2019-06-28'
     },
     {
       'id': '2',
-      'teacher': 'Happy王子乐',
-      'img': '../../../static/images/dsxcx/dsxcx.jpg',
-      'title': '企业级电商系统',
-      'remark': '电商小程序分为三端：微信小程序展示、vue管理后台、Java逻辑后台。采用当前流行技术，包括：Html5、CSS、JS、WeUI、Vue、ElementUI、Java、MySQL、Spring-boot。',
+      'teacher': '转载',
+      'img': '../../../static/images/project/weui/weui1.jpg',
+      'title': 'WeUI for 小程序',
+      'remark': 'WeUI for 小程序 为微信小程序量身设计。WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。包含button、cell、dialog、 progress、 toast、article、actionsheet、icon等各式元素。',
       'json': [
         {
           'type': 'project',
-          'url': 'https://pan.baidu.com/s/1wQ6TeSnm1_zIQVl6A211Lg',
-          'password': '77um'
+          'content': 'https://github.com/Tencent/weui-wxss/'
+        },
+        {
+          'type': 'img_big',
+          'content': ['../../../static/images/project/weui/weui1.jpg', 
+                      '../../../static/images/project/weui/weui2.jpg']
         }
       ],
-      'price': '600',
-      'time': '2019-06-01'
+      'time': '2019-06-27'
     },
     {
       'id': '3',
-      'teacher': 'Happy王子乐',
-      'img': '../../../static/images/dsxcx/dsxcx.jpg',
-      'title': '如何实现一个签到系统',
-      'remark': '电商小程序分为三端：微信小程序展示、vue管理后台、Java逻辑后台。采用当前流行技术，包括：Html5、CSS、JS、WeUI、Vue、ElementUI、Java、MySQL、Spring-boot。',
+      'teacher': '转载',
+      'img': '../../../static/images/project/wxapi/wxapi1.jpg',
+      'title': 'wxapi',
+      'remark': '微信小程序接口工具包，无需服务器，无需开发后台，开箱即用，轻松开发小程序',
       'json': [
         {
           'type': 'project',
-          'url': 'https://pan.baidu.com/s/1wQ6TeSnm1_zIQVl6A211Lg',
-          'password': '77um'
+          'content': 'https://github.com/gooking/wxapi'
+        },
+        {
+          'type': 'img_big',
+          'content': ['../../../static/images/project/wxapi/wxapi1.jpg', 
+                      '../../../static/images/project/wxapi/wxapi2.jpg']
         }
       ],
-      'price': '600',
-      'time': '2019-06-01'
+      'time': '2019-06-22'
     },
     {
       'id': '4',
-      'teacher': 'Happy王子乐',
-      'img': '../../../static/images/dsxcx/dsxcx.jpg',
-      'title': '后台管理系统模版',
-      'remark': '电商小程序分为三端：微信小程序展示、vue管理后台、Java逻辑后台。采用当前流行技术，包括：Html5、CSS、JS、WeUI、Vue、ElementUI、Java、MySQL、Spring-boot。',
+      'teacher': '转载',
+      'img': '../../../static/images/project/springboot/springboot1.jpg',
+      'title': 'spring boot 实践学习案例',
+      'remark': 'spring boot 实践学习案例，是 spring boot 初学者及核心技术巩固的最佳实践。',
       'json': [
         {
           'type': 'project',
-          'url': 'https://pan.baidu.com/s/1wQ6TeSnm1_zIQVl6A211Lg',
-          'password': '77um'
+          'content': 'https://github.com/JeffLi1993/springboot-learning-example'
+        },
+        {
+          'type': 'img_big',
+          'content': ['../../../static/images/project/springboot/springboot1.jpg', 
+                      '../../../static/images/project/springboot/springboot2.jpg',
+                      '../../../static/images/project/springboot/springboot3.jpg']
         }
       ],
-      'price': '600',
-      'time': '2019-06-01'
+      'time': '2019-06-22'
     },
     {
       'id': '5',
-      'teacher': 'Happy王子乐',
-      'img': '../../../static/images/dsxcx/dsxcx.jpg',
-      'title': '搜索系统入门Demo',
-      'remark': '电商小程序分为三端：微信小程序展示、vue管理后台、Java逻辑后台。采用当前流行技术，包括：Html5、CSS、JS、WeUI、Vue、ElementUI、Java、MySQL、Spring-boot。',
+      'teacher': '转载',
+      'img': '../../../static/images/blog/blog1.jpg',
+      'title': '炫酷博客模版',
+      'remark': '闲着无聊码的主页模版',
       'json': [
         {
           'type': 'project',
-          'url': 'https://pan.baidu.com/s/1wQ6TeSnm1_zIQVl6A211Lg',
-          'password': '77um'
+          'content': 'https://github.com/IceEnd/non-non-biyori'
+        },
+        {
+          'type': 'img_big',
+          'content': ['../../../static/images/project/blog/blog1.jpg', 
+                      '../../../static/images/project/blog/blog2.jpg',
+                      '../../../static/images/project/blog/blog3.jpg',
+                      '../../../static/images/project/blog/blog4.jpg']
         }
       ],
-      'price': '600',
-      'time': '2019-06-01'
+      'time': '2019-06-13'
     },
     {
       'id': '6',
-      'teacher': 'Happy王子乐',
-      'img': '../../../static/images/dsxcx/dsxcx.jpg',
-      'title': '搜索系统入门Demo',
+      'teacher': '转载',
+      'img': '../../../static/images/project/blog-major/blog-major1.jpg',
+      'title': '自定义博客主题模版',
       'remark': '电商小程序分为三端：微信小程序展示、vue管理后台、Java逻辑后台。采用当前流行技术，包括：Html5、CSS、JS、WeUI、Vue、ElementUI、Java、MySQL、Spring-boot。',
       'json': [
         {
           'type': 'project',
-          'url': 'https://pan.baidu.com/s/1wQ6TeSnm1_zIQVl6A211Lg',
-          'password': '77um'
+          'content': 'https://github.com/Cyclone77/Major'
+        },
+        {
+          'type': 'img_big',
+          'content': ['../../../static/images/project/blog-major/blog-major1.jpg', 
+                      '../../../static/images/project/blog-major/blog-major2.jpg',
+                      '../../../static/images/project/blog-major/blog-major3.jpg']
         }
       ],
-      'price': '600',
-      'time': '2019-06-01'
+      'time': '2019-06-07'
     }
   ]
 }
--- static/js/tool.js
+++ static/js/tool.js
@@ -4,7 +4,7 @@ const tool = {
       'id': '1',
       'teacher': '转载',
       'img': '../../../static/images/tool/idea.jpg',
-      'title': 'IDEA',
+      'title': 'IDEA下载',
       'remark': 'IDEA 全称 IntelliJ IDEA，是java编程语言开发的集成环境。IntelliJ在业界被公认为最好的java开发工具之一。站主建议java开发工具首选IDEA，相对使用Eclipse来说，熟练后开发效率会加快很多。',
       'json': [
         {
@@ -23,7 +23,7 @@ const tool = {
       'id': '2',
       'teacher': '转载',
       'img': '../../../static/images/tool/mysql.jpg',
-      'title': 'MySQL',
+      'title': 'MySQL下载',
       'remark': 'MySQL是一个关系型数据库管理系统，目前属于 Oracle 旗下产品。MySQL 是最流行的关系型数据库管理系统之一，在 WEB 应用方面，MySQL是最好的 RDBMS (Relational Database Management System，关系数据库管理系统) 应用软件之一。',
       'json': [
         {
@@ -42,7 +42,7 @@ const tool = {
       'id': '3',
       'teacher': '转载',
       'img': '../../../static/images/tool/redis1.jpg',
-      'title': 'Redis',
+      'title': 'Redis下载',
       'remark': 'Redis是一个开源的、可基于内存亦可持久化的日志型、Key-Value数据库，提供了多种语言的API。',
       'json': [
         {
@@ -61,7 +61,7 @@ const tool = {
       'id': '4',
       'teacher': '转载',
       'img': '../../../static/images/tool/jdk.jpg',
-      'title': 'JDK',
+      'title': 'JDK下载',
       'remark': 'JDK是 Java 语言的软件开发工具包，主要用于移动设备、嵌入式设备上的java应用程序。JDK是整个java开发的核心，它包含了JAVA的运行环境（JVM+Java系统类库）和JAVA工具。',
       'json': [
         {
@@ -80,7 +80,7 @@ const tool = {
       'id': '5',
       'teacher': '转载',
       'img': '../../../static/images/tool/navicat1.jpg',
-      'title': 'Navicat',
+      'title': 'Navicat下载',
       'remark': 'Navicat是一套快速、可靠并价格相当便宜的数据库管理工具，专为简化数据库的管理及降低系统管理成本而设。它的设计符合数据库管理员、开发人员及中小企业的需要。Navicat 是以直觉化的图形用户界面而建的，让你可以以安全并且简单的方式创建、组织、访问并共用信息。',
       'json': [
         {
@@ -99,7 +99,7 @@ const tool = {
       'id': '6',
       'teacher': '转载',
       'img': '../../../static/images/tool/vscode1.jpg',
-      'title': 'Visual Studio Code',
+      'title': 'Visual Studio Code下载',
       'remark': '一个运行于 Mac OS X、Windows和 Linux 之上的，针对于编写现代 Web 和云应用的跨平台源代码编辑器。',
       'json': [
         {
@@ -118,7 +118,7 @@ const tool = {
       'id': '7',
       'teacher': '转载',
       'img': '../../../static/images/tool/node1.jpg',
-      'title': 'Node.js',
+      'title': 'Node.js下载',
       'remark': 'Node.js 是一个基于 Chrome V8 引擎的 JavaScript 运行环境。',
       'json': [
         {
